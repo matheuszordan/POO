e12b1222a081cddfbf59666113a29c953c3b6dc6
matheuszordan <63318115+matheuszordan@users.noreply.github.com>
2021-03-30 10:13:54 -0300

    Add files via upload

---
+++ Lista02.Aula03/src/br/ucb/lista02/exercicio1/Conta.java
@@ -0,0 +1,21 @@
+package br.ucb.lista02.exercicio1;
+
+public class Conta {
+
+	String Titular;
+	float Numero;
+	float Agencia;
+	float saldo;
+	double dataAbertura;
+	
+	
+	
+	
+	public static float sacar(float s) {
+		float sacar;
+		
+		
+		
+		return s;
+	}
+}
